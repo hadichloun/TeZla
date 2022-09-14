c312068b07f223e1714b3f4ceec966ff5a1da01d
rails --version <hadichloun29@gmail.com>
2022-09-13 20:08:02 -0500

    intial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function header() {
+  return (
+    <div className='header'>
+      <div className="header__logo"></div>
+    </div>
+  )
+}
+
+export default Header
